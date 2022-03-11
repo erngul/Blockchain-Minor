8206105514b433d5baa7ef47cd9bf00aef89a383
Errow58 <erengul321@gmail.com>
2022-03-11 21:51:41 +0100

    homework

--- L03 Blockchain/excersise/Blockchain.py
+++ L03 Blockchain/excersise/Blockchain.py
@@ -1,10 +1,30 @@
+from cryptography.hazmat.primitives import hashes
+import inspect
+from Blockchain_t import anotherClass
+
 class CBlock:
     previousHash = None
     previousBlock = None
     data = None
     
     def __init__(self, data, previousBlock):
-        pass        
-    
+        print(inspect.isclass(anotherClass))
+        self.data = data
+        self.previousBlock = previousBlock
+        if previousBlock is not None:
+            self.previousHash = previousBlock.computeHash()
+
     def computeHash(self):
-        return 'aaa'
+        print(self.data)
+        if isinstance(self.data, int):
+            data = str(self.data).encode()
+        elif isinstance(self.data, str):
+            data = self.data.encode()
+        elif isinstance(self.data, (bytes, bytearray)):
+            data = self.data
+        elif isinstance(self.data, anotherClass):
+            data = self.data.string.encode()
+        digest = hashes.Hash(hashes.SHA256())
+        digest.update(data)
+        final = digest.finalize()
+        return final

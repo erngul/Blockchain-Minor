2fedc93bcdf5b2bca0cd265bc23ebc5093a490d9
Eren Gül <erengul321@gmail.com>
2022-03-13 14:27:45 +0100

    homework and excersis

--- L03 Blockchain/excersise/Blockchain.py
+++ L03 Blockchain/excersise/Blockchain.py
@@ -1,30 +1,22 @@
 from cryptography.hazmat.primitives import hashes
 import inspect
-from Blockchain_t import anotherClass
 
 class CBlock:
     previousHash = None
     previousBlock = None
     data = None
-    
     def __init__(self, data, previousBlock):
-        print(inspect.isclass(anotherClass))
         self.data = data
         self.previousBlock = previousBlock
         if previousBlock is not None:
             self.previousHash = previousBlock.computeHash()
 
     def computeHash(self):
-        print(self.data)
-        if isinstance(self.data, int):
-            data = str(self.data).encode()
-        elif isinstance(self.data, str):
-            data = self.data.encode()
-        elif isinstance(self.data, (bytes, bytearray)):
-            data = self.data
-        elif isinstance(self.data, anotherClass):
-            data = self.data.string.encode()
-        digest = hashes.Hash(hashes.SHA256())
-        digest.update(data)
-        final = digest.finalize()
-        return final
+        global hash1
+        try:
+            hash1 = (str(self.data.string) + str(self.data.num))
+        except:
+            hash1 = str(self.data)
+        if self.previousHash is not None:
+            hash1 = hash1 + str(self.previousBlock.computeHash())
+        return hash(hash1)
--- L03 Blockchain/homework/BlockChain.py
+++ L03 Blockchain/homework/BlockChain.py
@@ -1,10 +1,44 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
+import hashlib
+
+
 
 class CBlock:
-        
+    previousHash = None
+    CurrentHash = None
+    previousBlock = None
+    data = None
+    Nonce = None
+    def __init__(self, data, previousBlock=None):
+        self.data = data
+        self.previousBlock = previousBlock
+        self.CurrentHash = self.sha256(data)
+        self.Nonce = 1
+        if previousBlock is not None:
+            self.previousHash = self.previousBlock.CurrentHash
+
+
+    def sha256(self, message):
+        return hashlib.sha256(message.encode('UTF-8')).hexdigest()
+
     def mine(self, leading_zeros):
-        pass
+        prefix = '0' * leading_zeros
+        if self.previousBlock is not None:
+            self.previousHash = self.previousBlock.CurrentHash
+        for i in range(1000000):
+            self.Nonce = i
+            digest = str(self.data) + str(i)
+            if self.previousBlock is not None:
+                digest += str(self.previousHash)
+            digest = self.sha256(digest)
+            if digest.startswith(prefix):
+                self.CurrentHash = digest
+                return
 
     def is_valid_hash(self):
-        pass
+        digest = str(self.data) + str(self.Nonce)
+        if self.previousBlock is not None:
+            digest += str(self.previousHash)
+        newHash = self.sha256(digest)
+        if  newHash == self.CurrentHash:
+            return True
+        return False
